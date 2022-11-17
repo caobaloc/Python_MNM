6d62c5d043f8ccb1e0052999c35d145da9a4152e
caobaloc <58618441+caobaloc@users.noreply.github.com>
2022-11-17 21:47:52 +0700

    Python

---
+++ NikeStore/NikeStore/home/component/footer.php
@@ -0,0 +1,8 @@
+<div class="footer-content bg-pink text-white mt-5">
+    <div class="container py-3">
+        <h5 class="copyright__title" style="font-weight:bold;">
+            @ CÔNG TY TNHH NIKE STORE
+        </h5> 
+        <p class="copyright-description text-white">Mã số doanh nghiệp: 0109902052. <br> Giấy chứng nhận đăng ký doanh nghiệp do Sở Kế hoạch và Đầu tư TP Hà Nội cấp lần đầu ngày 07/02/2022.</p>
+    </div>
+</div>
